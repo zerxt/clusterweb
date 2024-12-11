5e0ed3340ab58dcb105456a3c84626be6d86a159
matteo server <matthdserver@gmail.com>
2024-12-11 23:25:13 +0700

    sebagian besar sudah selesai

--- products/forms.py
+++ products/forms.py
@@ -1,8 +1,16 @@
 # products/forms.py
 from django import forms
 from .models import Product
+from .models import Transaction
+
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['product_name', 'product_photo', 'product_type', 'product_price', 'product_weight']
+
+
+class BuyProductForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['quantity', 'payment_method']
